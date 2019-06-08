dc99cfce57597dd21b8689c2cbed0e18896b63fa
wanz <39509865+pandorawanz@users.noreply.github.com>
2019-06-08 23:54:05 +0800

    create same-named classes in different package (#15)
    create same-named classes in different package (#15)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,9 +1,8 @@
 package com.github.hcsp;
 
 import com.github.hcsp.pet1.Cat;
-import com.github.hcsp.pet2.Cat;
 
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    Cat cat1 = new Cat();
+    com.github.hcsp.pet2.Cat cat2 = new com.github.hcsp.pet2.Cat();
 }
