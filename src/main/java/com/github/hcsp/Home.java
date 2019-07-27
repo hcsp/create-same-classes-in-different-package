a1ee57804872102ac861d1dfb5d2de0f7687ab6d
吕子威 <18500905537@163.com>
2019-07-27 17:51:36 +0800

    使用全路径方法，来解决如何引入2个不同package但是同名Class的问题 (#61)
    使用全路径方法，来解决如何引入2个不同package但是同名Class的问题 (#61)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,9 +1,8 @@
 package com.github.hcsp;
 
-import com.github.hcsp.pet1.Cat;
-import com.github.hcsp.pet2.Cat;
-
 public class Home {
-    Cat cat1;
-    Cat cat2;
+    com.github.hcsp.pet1.Cat cat1;
+    com.github.hcsp.pet2.Cat cat2;
+
 }
+
