33b90ac6ea015f17ff8198f3a36a89ffae72b617
ambition-to <dafu.wdf.dev@gmail.com>
2019-07-30 10:47:17 +0800

    feat: 1. 修改 Cat 以符合全限定类名

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,9 +1,14 @@
 package com.github.hcsp;
 
 import com.github.hcsp.pet1.Cat;
-import com.github.hcsp.pet2.Cat;
+//import com.github.hcsp.pet2.Cat;
 
 public class Home {
     Cat cat1;
-    Cat cat2;
+    com.github.hcsp.pet2.Cat cat2;
+
+    public Home() {
+        this.cat1 = new Cat();
+        this.cat2 = new com.github.hcsp.pet2.Cat();
+    }
 }
