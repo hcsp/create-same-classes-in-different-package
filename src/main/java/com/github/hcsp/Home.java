08ee082f52e4db2a2d00d02d3c4ad036561e90f5
SUPER Administrator <1276119527@qq.com>
2019-09-30 14:59:05 +0800

    引用成功

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -2,9 +2,9 @@ package com.github.hcsp;
 
 
 
-import com.github.hcsp.pet2.Cat;
+import com.github.hcsp.pet1.Cat;
 
 public class Home {
     Cat cat1;
-    com.github.hcsp.pet1.Cat cat2;
+    com.github.hcsp.pet2.Cat cat2;
 }
