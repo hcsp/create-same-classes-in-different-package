e5bfbb153fdd42e719618b02636258ffd46db208
TangsongyuanmingQingHad <had13625428701@aliyun.com>
2019-12-07 09:34:59 +0800

    给其中的一个Cat加上了前缀来标识引用的具体包

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,8 +1,6 @@
 package com.github.hcsp;
 
-import com.github.hcsp.pet1.Cat;
-
 public class Home {
-    com.github.hcsp.pet2.Cat cat1;
-    Cat cat2;
+    com.github.hcsp.pet1.Cat cat1;
+    com.github.hcsp.pet2.Cat cat2;
 }
