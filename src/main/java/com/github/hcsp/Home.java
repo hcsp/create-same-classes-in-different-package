cd884e5388bc94634d7be8ef640ec2ffd03dbe50
Riccco <32475423+Riccco@users.noreply.github.com>
2021-12-27 15:31:07 +0800

    完成不同类名的引入 (#311)
    完成不同类名的引入 (#311)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,9 +1,10 @@
 package com.github.hcsp;
 
 import com.github.hcsp.pet1.Cat;
-import com.github.hcsp.pet2.Cat;
+
+// 当两个类发生命名冲突时你可以使用全限定类名来引入其他类
 
 public class Home {
     Cat cat1;
-    Cat cat2;
+    com.github.hcsp.pet2.Cat cat2;
 }
