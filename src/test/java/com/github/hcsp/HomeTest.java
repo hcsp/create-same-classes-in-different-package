92eb787ccb4930499fe4b3a394bc3ce059395fec
yangbisheng <2606424906@qq.com>
2022-03-17 08:19:34 +0800

    引用同名不同类

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 public class HomeTest {
     @Test
-    public void homeHasTwoKindsOfCat() {
+    public void
+    homeHasTwoKindsOfCat() {
         Home home = new Home();
         home.cat1 = new com.github.hcsp.pet1.Cat();
         home.cat2 = new com.github.hcsp.pet2.Cat();
